ca6625445b13c82cda2a0b38765b94f983a3d04b
nguyendoquangtrong <23dh113770@st.huflit.edu.vn>
2025-12-04 17:18:19 +0700

    src: uncorver update

---
+++ src/Services/Catalog/Catalog.API/Program.cs
@@ -0,0 +1,20 @@
+using Marten;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container
+builder.Services.AddCarter();
+builder.Services.AddMediatR(config =>
+{
+    config.RegisterServicesFromAssembly(typeof(Program).Assembly);
+});
+builder.Services.AddMarten(opts =>
+{
+    opts.Connection(builder.Configuration.GetConnectionString("Database")!);
+}).UseLightweightSessions();
+
+var app = builder.Build();
+
+// configure the HTTP request pipeline
+app.MapCarter();
+app.Run();
