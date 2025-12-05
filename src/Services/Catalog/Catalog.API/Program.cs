317ec39c857a372e51672da99fd539ea1fe4c634
nguyendoquangtrong <23dh113770@st.huflit.edu.vn>
2025-12-06 01:06:01 +0700

    src: Update 1:05 6/12/2025

--- src/Services/Catalog/Catalog.API/Program.cs
+++ src/Services/Catalog/Catalog.API/Program.cs
@@ -1,20 +1,23 @@
-using Marten;
+using BuildingBlocks.Behaviors;
 
 var builder = WebApplication.CreateBuilder(args);
+var assembly = typeof(Program).Assembly;
+
 
 // Add services to the container
-builder.Services.AddCarter();
 builder.Services.AddMediatR(config =>
 {
-    config.RegisterServicesFromAssembly(typeof(Program).Assembly);
+    config.RegisterServicesFromAssembly(assembly);
+    config.AddOpenBehavior(typeof(ValidationBehavior<,>));
 });
+builder.Services.AddValidatorsFromAssembly(assembly);
 builder.Services.AddMarten(opts =>
 {
     opts.Connection(builder.Configuration.GetConnectionString("Database")!);
 }).UseLightweightSessions();
-
-var app = builder.Build();
+builder.Services.AddCarter();
 
 // configure the HTTP request pipeline
+var app = builder.Build();
 app.MapCarter();
 app.Run();
