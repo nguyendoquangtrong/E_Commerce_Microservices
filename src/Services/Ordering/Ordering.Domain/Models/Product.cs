9ad9a88f71df4d2c62251eb532e37724872b73a5
nguyendoquangtrong <23dh113770@st.huflit.edu.vn>
2025-12-31 23:36:58 +0700

    src: Update(Ordering.Domain) 23:36 31/12/2025

--- src/Services/Ordering/Ordering.Domain/Models/Product.cs
+++ src/Services/Ordering/Ordering.Domain/Models/Product.cs
@@ -3,17 +3,17 @@ namespace Ordering.Domain.Models;
 public class Product : Entity<ProductId>
 {
     public string Name { get; private set; } = default!;
-    public string Email { get; private set; } = default!;
+    public decimal Price { get; private set; } = default!;
 
-    public static Product Create(ProductId id, string name, string email)
+    public static Product Create(ProductId id, string name, decimal price)
     {
         ArgumentException.ThrowIfNullOrWhiteSpace(name);
-        ArgumentException.ThrowIfNullOrWhiteSpace(email);
+        ArgumentOutOfRangeException.ThrowIfNegativeOrZero(price);
         var product = new Product
         {
             Id = id,
             Name = name,
-            Email = email
+            Price = price
         };
         return product;
     }
